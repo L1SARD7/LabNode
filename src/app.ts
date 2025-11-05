8697417529c99dcec4ef663ef7f768d4525e4666
L1SARD7 <arthemklimchuk7@gmail.com>
2025-11-05 09:30:15 +0200

    feat: setup ejs view engine and static files configuration

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
 import express from 'express';
+import path from 'path';
 import { GamesRouter } from './routers/game-router';
 
 export const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+app.use(express.static(path.join(__dirname, '../front')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/games', GamesRouter);
+
+app.get('/', (req, res) => {
+    res.redirect('/games');
+});
